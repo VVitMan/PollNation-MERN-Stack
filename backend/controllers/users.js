1599e2d541675d60c0f82153bf625e577f660e05
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-10 15:29:25 +0700

    add comment in controllers/users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,12 +1,21 @@
+// Import the User model from the User.js file
 import User from "../models/User.js";
 
 /* READ */
-export const getUser = async(req, res) => {
+
+// Define an asynchronous function to get user data
+export const getUser = async (req, res) => {
     try {
+        // Destructure the 'id' parameter from the request URL (e.g., /user/:id)
         const { id } = req.params;
+
+        // Use the User model to find a user by their unique ID
         const user = await User.findById(id);
+
+        // If the user is found, respond with a 200 status code and the user data in JSON format
         res.status(200).json(user);
     } catch (err) {
+        // If an error occurs, respond with a 404 status code and an error message
         res.status(404).json({ message: err.message });
     }
 }
