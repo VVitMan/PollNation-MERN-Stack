e2c6a14e1ba88ee32034693f4c59982c38e2cc3c
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-11 20:45:10 +0700

    set up navbar at home page

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,6 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "../navbar";
+
 const HomePage = () => {
     return (
-        <div>home pages</div>
+        <Box>
+            <Navbar />
+        </Box>
     )
 }
 
