cf3ec14d87135b125529257e319204f3a351368a
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-12 22:54:33 +0700

    reset backend

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
-
-const router = express.Router();
-
-router.post("/login", login); // /auth/login that we assign in index.js
-
-export default router;
