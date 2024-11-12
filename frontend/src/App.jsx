0fccbb88cf028c11c4318a6c90d85aebde414bd2
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-12 23:44:34 +0700

    set up path in frontend, home, sigin, signup, community

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,41 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar/Navbar.jsx';
-import Poll from './Poll/Poll.jsx';
-import ProfilePage from './ProfilePage/ProfilePage.jsx'; // Import the ProfilePage component
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Community from "./pages/Community";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="main-container">
-      <Navbar />
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Poll />} /> Main Poll Page
-        <Route path="/profile/:username" element={<ProfilePage />} /> {/* Profile Page */}
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
-      </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
-export default App;
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './Navbar/Navbar.jsx';
+// import Poll from './Poll/Poll.jsx';
+// import ProfilePage from './ProfilePage/ProfilePage.jsx'; // Import the ProfilePage component
 
+// function App() {
+//   return (
+//     <Router>
+//       <div className="main-container">
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Poll />} /> Main Poll Page
+//         <Route path="/profile/:username" element={<ProfilePage />} /> {/* Profile Page */}
+//       </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
