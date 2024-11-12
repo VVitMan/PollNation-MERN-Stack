baf1ae5f0557b1d957b92039ce78df76274c2493
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-12 22:48:20 +0700

    change title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="main-container">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Poll />} /> {/* Main Poll Page */}
+        <Route path="/" element={<Poll />} /> Main Poll Page
         <Route path="/profile/:username" element={<ProfilePage />} /> {/* Profile Page */}
       </Routes>
       </div>
@@ -19,27 +19,3 @@ function App() {
 
 export default App;
 
-
-/*
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        Test Hello World!
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-*/
