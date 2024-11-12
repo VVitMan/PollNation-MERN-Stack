c930ebcddac222d87354f9982402f0889288d0f3
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-13 00:12:57 +0700

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -6,3 +9,13 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
+
+/* Connect MongoDB */
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connect to MongoDB");
+    })
+    // use "err" for dev phase
+    .catch((err) => {
+        console.log(err);
+    })
