756239c637c7b63c374806ebf4a1937722474780
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-12 18:57:22 +0700

    try login in new branch

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,39 +2,12 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
     {
-        firstName: {
-            type: String,
-            required: true,
-            min: 2,
-            max: 50
-        },
-        lastName: {
-            type: String,
-            required: true,
-            min: 2,
-            max: 50
-        },
-        username: {
-            type: String,
-            required: true,
-            min: 2,
-            max: 50
-        },
-        email: {
-            type: String,
-            required: true,
-            max: 50,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true,
-            min: 8
-        },
-        picturePath: {
-            type: String,
-            default: "",
-        },
+        firstName: { type: String, required: true, min: 2, max: 50 },
+        lastName: { type: String, required: true, min: 2, max: 50 },
+        username: { type: String, required: true, min: 2, max: 50 },
+        email: { type: String, required: true, max: 50, unique: true },
+        password: { type: String, required: true, min: 8 },
+        picturePath: { type: String, default: "", },
     },
     {timestamps: true}
 )
