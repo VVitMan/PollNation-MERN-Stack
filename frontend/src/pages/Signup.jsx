e1c05549e41f1a1756401f9438ea5dcde0d32db9
VVitMan <ithikorn.amornkhana@gmail.com>
2024-11-13 18:19:14 +0700

    add redux toolkit

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -30,7 +30,6 @@ export default function SignUp() {
         body: JSON.stringify(formData)
       });
       const data = await res.json();
-      console.log(data);
       setLoading(false);
       // fetch need to have it
       if (data.success === false) {
